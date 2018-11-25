64c6de93a96c2ed5edc584b85d9dc96c8a0c5551
Enrico Tassi <Enrico.Tassi@Inria.fr>
2018-11-25 21:49:23 +0100

    fix ex 4

--- exercise4.v
+++ exercise4.v
@@ -2,28 +2,35 @@ From mathcomp Require Import all_ssreflect.
 
 Implicit Type P Q R : Prop.
 
-(** *** Exercise 1:
-    - Declare exists2
-    - Prove a lemma ex1 -> ex2 T
+(** *** Exercise 0:
+    - Define not. In type theory negation is defined in terms
+      of [False].
 *)
 
 Definition not P := 
 (*D*) P -> False
 .
 
+(** *** Exercise 1:
+    - Prove the negation of the excluded middle.
+*)
 Lemma ex0 P : not (P /\ not P).
 Proof.
 (*D*)by move=> [p np]; apply: np; apply p.
 Qed.
 
+(** *** Exercise 2:
+    - Declare iff (the constructor being called [iff_intro]).
+*)
 Inductive iff P Q :=
 (*D*)  iff_intro (p2q : P -> Q) (q2p : Q -> P)
 .
 
+
 Definition iff1 P Q : iff P Q -> P -> Q :=
   fun x => match x with iff_intro f _ => f end.
 
-Inductive xor P Q :=
+Inductive xor P Q : Prop :=
 (*D*) | xorL (p : P) (np : not Q)
 (*D*) | xorR (no : not P) (q : Q)
 .
